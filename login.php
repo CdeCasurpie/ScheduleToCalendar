577dee82c7fb1a3d3f9daf14d8d12dfbf428d15e
CdeCasurpie <c.casurpie@gmail.com>
2023-12-09 14:08:21 -0500

    a

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "POST- login page";
 } else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // mostrar la pagina de login que esta en login.html
     echo "GET- login page";
 }
 ?>
