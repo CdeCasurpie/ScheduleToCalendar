e0611cd4235e33ac7710c3a17c07a86bc7c4e8ce
CdeCasurpie <c.casurpie@gmail.com>
2023-12-09 14:13:52 -0500

    prueba

--- login.php
+++ login.php
@@ -4,6 +4,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "POST- login page";
 } else if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // mostrar la pagina de login que esta en login.html
-    echo "GET- login page";
+    include "login.html";
 }
 ?>
